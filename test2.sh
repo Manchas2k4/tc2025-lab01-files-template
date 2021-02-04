fea0475bc11b3778e6eaad794f381a97d28e75d7
Pedro Perez <pperezm@tec.mx>
2021-02-04 17:16:49 -0600

    test modified to best retro

--- test2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# If the test runs successfully, that's 15 additional points.
-
-gcc $1 -o app
-./app
-if [[ $? -ne 255 ]] ; then 
-	exit 1
-fi
--- test3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# If the test runs successfully, that's 15 additional points.
-
-gcc $1 -o app
-./app dont_exist output.txt
-if [[ $? -ne 254 ]] ; then 
-	exit 1
-fi
