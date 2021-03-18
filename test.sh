2da69f6fed30eef782d12060cbdd169edbdfd2fd
Pedro Perez <pperezm@tec.mx>
2021-03-18 14:25:38 -0600

    test changed: it is no necesary to modify the input.txt

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+rm app temp.txt
+
 sed -i 's/\r//g' $1
-sed -i 's/\r//g' input.txt
-sed -i 's/\r//g' output.txt 
+sed -i 's/\r//g' output.txt
 
 cal=0
 
 echo "Compiling..."
 gcc $1 -o app
-if [[ $? -ne 0 ]] ; then 
+if [[ $? -ne 0 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -17,7 +18,7 @@ cal=$((cal+10))
 
 echo "Checking parameters..."
 ./app > /dev/null 2>&1
-if [[ $? -ne 255 ]] ; then 
+if [[ $? -ne 255 ]] ; then
 	echo "grade: $cal"
 	rm app
 	exit 1
@@ -27,7 +28,7 @@ echo " done"
 
 echo "Verifying that the input file exists..."
 ./app dont_exist output.txt > /dev/null 2>&1
-if [[ $? -ne 254 ]] ; then 
+if [[ $? -ne 254 ]] ; then
 	echo "grade: $cal"
 	rm app
 	exit 1
@@ -39,7 +40,7 @@ echo "Verifying the operation of the program..."
 ./app input.txt temp.txt > /dev/null 2>&1
 sed -i 's/\r//g' temp.txt
 diff temp.txt output.txt > /dev/null 2>&1
-if [[ $? -ne 0 ]] ; then 
+if [[ $? -ne 0 ]] ; then
 	echo "grade: $cal"
 	rm app temp.txt
 	exit 1
@@ -48,4 +49,3 @@ echo " done"
 
 cal=$((cal+60))
 echo "=>grade: $cal"
-rm app temp.txt
